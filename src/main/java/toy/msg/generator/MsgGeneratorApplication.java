1f3146de355db520c8b7625ce5ffce123e5f0933
M1nsong-Kim <lgms.3222@gmail.com>
2023-05-20 22:28:47 +0900

    프로젝트 시작

---
+++ src/main/java/toy/msg/generator/MsgGeneratorApplication.java
@@ -0,0 +1,13 @@
+package toy.msg.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgGeneratorApplication.class, args);
+	}
+
+}
